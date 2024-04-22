197a065df8d1d432b77d05ed92c1e8b86780a9a4
King Ibrahim <162009674+ibrahimaitech@users.noreply.github.com>
2024-04-22 22:00:44 +0300

    Update config.js

--- config.js
+++ config.js
@@ -103,7 +103,7 @@ global.save_status_from =  process.env.SAVE_STATUS_FROM  || "254xxxxxxxxx";
 global.read_status_from =  process.env.READ_STATUS_FROM  ||  "254710772666,254739937062";
 
 global.api_smd = "https://api-smd.onrender.com" //  || "https://api-smd-1.vercel.app" // expires
-global.wachannel = "https://whatsapp.com/channel/0029VaZuGSxEawdxZK9CzM0Y";
+global.scan = "https://black-panther-scanner-app-a00dbf372b79.herokuapp.com/";
 
 
 global.SESSION_ID = process.env.SESSION_ID ||  "" ; // PUT your SESSION_ID 
